06f35c55ff3cb592c6d9aec72ebd8a67f63d476e
Antonio Gennaro <antgennaro@hotmail.com>
2023-09-03 10:43:11 -0300

    Ej 1.1 matrices en JS- numeros up to 100

--- mat_ej1.js
+++ mat_ej1.js
@@ -1,5 +1,5 @@
 // Ejerco 1 matrices con char
-
+/*
 const CODIGO_A_MIN  = 97;
 const CODIGO_Z_MIN  = 122;
 const matrizChar    = [];
@@ -17,3 +17,25 @@ for (let i=0; i < 6; i++) {
 }
 
 console.table(matrizChar);
+*/
+// Ejercicio 2 matrices con números
+
+const NRO_FILAS     = 10;
+const NRO_COLUMNAS  = 10;
+const MAX_ELEM      = 100;
+const matrizNum     = [];
+let   numero        = 1;
+
+for (let i=0; i < NRO_FILAS; i++) {
+    matrizNum.push([]);
+    for (let j=0; j < NRO_COLUMNAS;j++) {
+        matrizNum[i].push(numero);
+        numero++;
+        if (numero > MAX_ELEM ) {
+            j=NRO_COLUMNAS;
+        }
+    }
+}
+
+console.table(matrizNum);
+
